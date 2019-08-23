ac4bd85ac57a7094ed78646bead8758f21d10fe0
Veronica Santos <verosbraga@gmail.com>
2019-08-23 17:54:38 +0100

    Controllers and Views Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  # get 'doses/new'
+  # get 'doses/create'
+  # get 'doses/destroy'
+  # get 'cocktails/index'
+  # get 'cocktails/show'
+  # get 'cocktails/new'
+  # get 'cocktails/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create]
